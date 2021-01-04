35053429f1dfe8cbc3b5d54267f8221685cd46e5
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-04 13:28:02 -0800

    add grid paper

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -4,13 +4,6 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-export const Product = () => {
-    return(
-        <div>
-         <h4 className="proHead">Discover the Collection of Men's Shirts </h4>
-         </div>
-    )
-}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -25,34 +18,20 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-export default function CenteredGrid() {
-  const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
+export const Product = () => {
+    const classes = useStyles();
+    return(
+        <div>
+         <h4 className="proHead">Discover the Collection of Men's Shirts </h4>
+
+          <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <Paper className={classes.paper}>xs=12</Paper>
         </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
       </Grid>
     </div>
-  );
+         </div>
+    )
 }
