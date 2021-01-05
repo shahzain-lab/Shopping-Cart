e82ee9662b9048fca73028712da69816e7d33419
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-05 10:47:52 -0800

    add gucci img

--- src/components/ProductItem/ProItem.tsx
+++ src/components/ProductItem/ProItem.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { ProductTypes } from '../../types/Producttypes';
+//imgs
+import gucciCart from '../../images/gucciCart.jpg';
+
+interface ProductType{
+    products: ProductTypes[];
+}
 
 export const ProductItem = () => {
+   
+    const { products } = useSelector((state: ProductType) => state);
+   const productFilter = products.filter(
+     product => product.added === true
+   )
     return (
         <div>
-            
+           <div>
+               <img src={gucciCart} alt="gucci"/>
+               </div>         
         </div>
     )
 }
